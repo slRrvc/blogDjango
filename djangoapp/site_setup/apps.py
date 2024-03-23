8294653237dc935ae0a8fb488ee8ee81809ba3d7
slRrvc <slrvcvieira@gamil.com>
2024-03-23 13:55:34 -0400

    Atualizando arquivos

--- djangoapp/site_setup/apps.py
+++ djangoapp/site_setup/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SiteSetupConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'site_setup'
+    verbose_name = 'Configurações do Blog'
--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -3,12 +3,37 @@ from django.db import models
 
 class MenuLink(models.Model):
     class Meta:
-        verbose_name = 'Menu Link'
-        verbose_name_plural = 'Menu Links'
+        verbose_name = 'Link'
+        verbose_name_plural = 'Links'
 
     text = models.CharField(max_length=50, verbose_name='Testo do link')
     url_or_path = models.CharField(max_length=2048, verbose_name='Url ou Caminho')
     new_tab = models.BooleanField(default=False, verbose_name='Abrir em nova aba')
+    site_setup = models.ForeignKey(
+        'SiteSetup', 
+        on_delete = models.CASCADE,
+        blank= True,
+        null = True,
+        default = None,
+        verbose_name= 'Site'
+    )
 
     def __str__(self):
         return self.text
+    
+class SiteSetup(models.Model):
+    class Meta:
+        verbose_name = 'Site'
+        verbose_name_plural = 'Site'
+
+    title = models.CharField(max_length=65, verbose_name = 'Titulo do Site')
+    description = models.CharField(max_length=255, verbose_name = 'Descrição')
+    show_header = models.BooleanField(default=True, verbose_name = 'Mostrar cabeçalho')
+    show_search = models.BooleanField(default=True, verbose_name = 'Mostrar pesquisa')
+    show_menu = models.BooleanField(default=True, verbose_name = 'Mostrar menu')
+    show_description = models.BooleanField(default=True, verbose_name = 'Mostrar descrição')
+    show_pagination = models.BooleanField(default=True, verbose_name = 'Mostrar paginação')
+    show_footer = models.BooleanField(default=True, verbose_name = 'Mostrar rodapé')
+
+    def __str__(self):
+        return self.title
