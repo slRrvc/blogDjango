d45ca1407a413bb259b3d424c651c1f175bd3a5c
slRrvc <slrvcvieira@gamil.com>
2024-03-26 19:15:13 -0400

    Atualizando arquivos css

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from utils.model_validators import velidate_png
+from utils.images import resize_image
+
 
 class MenuLink(models.Model):
     class Meta:
@@ -7,45 +9,60 @@ class MenuLink(models.Model):
         verbose_name_plural = 'Links'
 
     text = models.CharField(max_length=50, verbose_name='Testo do link')
-    url_or_path = models.CharField(max_length=2048, verbose_name='Url ou Caminho')
-    new_tab = models.BooleanField(default=False, verbose_name='Abrir em nova aba')
+    url_or_path = models.CharField(
+        max_length=2048, verbose_name='Url ou Caminho')
+    new_tab = models.BooleanField(
+        default=False, verbose_name='Abrir em nova aba')
     site_setup = models.ForeignKey(
-        'SiteSetup', 
-        on_delete = models.CASCADE,
-        blank= True,
-        null = True,
-        default = None,
-        verbose_name= 'Site'
+        'SiteSetup',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        default=None,
+        verbose_name='Site'
     )
 
     def __str__(self):
         return self.text
-    
+
+
 class SiteSetup(models.Model):
     class Meta:
         verbose_name = 'Site'
         verbose_name_plural = 'Site'
 
-    title = models.CharField(max_length=65, verbose_name = 'Titulo do Site')
-    description = models.CharField(max_length=255, verbose_name = 'Descrição')
-    show_header = models.BooleanField(default=True, verbose_name = 'Mostrar cabeçalho')
-    show_search = models.BooleanField(default=True, verbose_name = 'Mostrar pesquisa')
-    show_menu = models.BooleanField(default=True, verbose_name = 'Mostrar menu')
-    show_description = models.BooleanField(default=True, verbose_name = 'Mostrar descrição')
-    show_pagination = models.BooleanField(default=True, verbose_name = 'Mostrar paginação')
-    show_footer = models.BooleanField(default=True, verbose_name = 'Mostrar rodapé')
+    title = models.CharField(max_length=65, verbose_name='Titulo do Site')
+    description = models.CharField(max_length=255, verbose_name='Descrição')
+    show_header = models.BooleanField(
+        default=True, verbose_name='Mostrar cabeçalho')
+    show_search = models.BooleanField(
+        default=True, verbose_name='Mostrar pesquisa')
+    show_menu = models.BooleanField(default=True, verbose_name='Mostrar menu')
+    show_description = models.BooleanField(
+        default=True, verbose_name='Mostrar descrição')
+    show_pagination = models.BooleanField(
+        default=True, verbose_name='Mostrar paginação')
+    show_footer = models.BooleanField(
+        default=True, verbose_name='Mostrar rodapé')
 
-    favicon =models.ImageField(
+    favicon = models.ImageField(
         upload_to='assets/favicon/%Y/%m/',
         blank=True,
         default='',
         validators=[velidate_png],
     )
- 
+
     def save(self, *args, **kwargs):
         current_favicon_name = str(self.favicon.name)
-        
+
         super().save(*args, **kwargs)
+
+        favicon_changed = False
+        if self.favicon:
+            favicon_changed = current_favicon_name != self.favicon.name
+
+        if favicon_changed:
+            resize_image(self.favicon, 32)
 
     def __str__(self):
         return self.title
